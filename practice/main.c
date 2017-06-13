59e90c08f8c7cf9d0f12a0af870b4a5d73cd5c3d
20161104593 <a20161104593@King.local>
2017-06-13 09:36:04 +0800

    将数据放入文件

--- practice/main.c
+++ practice/main.c
@@ -10,6 +10,10 @@
 
 int main()
 {
+    FILE *fr;
+    FILE *fw;
+    fr=fopen("//Users//a20161104593//Desktop//practice//input.txt","w");
+    fw=fopen("//Users//a20161104593//Desktop//practice//output.txt","w");
     int i,j;
     int temp;
     int a[10];
@@ -17,6 +21,7 @@ int main()
     for(i=0;i<=9;i++)
     {
         scanf("%d",&a[i]);
+        fprintf(fr,"%d\n",a[i]);
     }
     for(j=1;j<=9;j++)
     {
@@ -24,15 +29,16 @@ int main()
         {
             if(a[i]>a[i+1])
             {
-                temp=a[i];
-                a[i]=a[i+1];
-                a[i+1]=temp;
+                    temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
             }
         }
     }
     for(i=0;i<=9;i++)
     {
         printf("%d\n",a[i]);
+        fprintf(fw,"%d\n",a[i]);
     }
     return 0;
 }
